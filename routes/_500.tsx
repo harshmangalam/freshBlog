a4164306dc935c7a19f4192208a105ec58f15f9b
yousaf wazir <umbrellaman612@gmail.com>
2022-07-17 18:26:52 +0100

    feat: added 404 page

---
+++ routes/_500.tsx
@@ -0,0 +1,12 @@
+/** @jsx h */
+import { h } from "preact";
+import { ErrorPageProps } from "$fresh/server.ts";
+
+export default function Error500Page({ error }: ErrorPageProps) {
+  return (
+    <p>
+      500 internal error: {(error as Error).message}{" "}
+      <a href="/">Head back home</a>
+    </p>
+  );
+}
