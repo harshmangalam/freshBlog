76dbbe724bcc8e8d889a2b80397c71be7448d183
yousaf wazir <umbrellaman612@gmail.com>
2022-07-17 18:25:54 +0100

    Revert "feat: added 404 and 500 pages"

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,17 +3,13 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/[slug].tsx";
-import * as $1 from "./routes/_404.tsx";
-import * as $2 from "./routes/_500.tsx";
-import * as $3 from "./routes/index.tsx";
+import * as $1 from "./routes/index.tsx";
 import * as $$0 from "./islands/ThemeSwitcher.tsx";
 
 const manifest = {
   routes: {
     "./routes/[slug].tsx": $0,
-    "./routes/_404.tsx": $1,
-    "./routes/_500.tsx": $2,
-    "./routes/index.tsx": $3,
+    "./routes/index.tsx": $1,
   },
   islands: {
     "./islands/ThemeSwitcher.tsx": $$0,
